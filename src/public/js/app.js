6722440d7958e7c1a7cc97a1848cb68b41a61278
UHyun-K <konglim2@naver.com>
2022-03-02 14:13:54 +0900

    nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,11 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const nameForm = welcome.querySelector("#name");
+const enterForm= welcome.querySelector("#enter");
 const room = document.getElementById("room");
+
+
 
 room.hidden=true;
 
@@ -16,38 +19,46 @@ function addMessage(msg){
 }
 function hanldeMessageSubmit(event){
     event.preventDefault();
-    const input = room.querySelector("input");
+    const input = room.querySelector("#msg input");
     const value = input.value;
     socket.emit("new_message", input.value, roomName, ()=>{
-        addMessage(`You ${value}`);
+        addMessage(`You: ${value}`);
     }); 
     input.value="";
 }
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = welcome.querySelector("#name input");
+    socket.emit("nickname", input.value);
+    nameForm.hidden=true;
+    const div = welcome.querySelector("div");
+    div.innerText=`반갑습니다 ${input.value}님`;
+
+}
+nameForm.addEventListener("submit", handleNicknameSubmit);
+
 function showRoom(msg){
     room.hidden=false;
     welcome.hidden = true;
     const h3 = room.querySelector("h3");
     h3.innerText= `Room ${roomName}`;
     const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
     msgForm.addEventListener("submit", hanldeMessageSubmit);
-    nameForm.addEventListener("submit", hanldeNicknameSubmit);
-
 }
 
 function hanldeRoomSubmit(event){
     event.preventDefault();
-    const input = form.querySelector("input");
+    const input = enterForm.querySelector("input");
     socket.emit("enter_room",input.value, showRoom  );
     roomName=input.value;
     input.value="";
 }
-form.addEventListener("submit", hanldeRoomSubmit);
+enterForm.addEventListener("submit", hanldeRoomSubmit);
 
-socket.on("welcome",()=>{
-    addMessage("Someone Joined!")
+socket.on("welcome", (user)=>{
+    addMessage(`${user}님이 입장하였습니다.` );
 });
-socket.on("bye",()=>{
-    addMessage("Someone Left!")
+socket.on("bye", (left)=>{
+    addMessage(` ${left}님이  나갔습니다.`);
 });
 socket.on("new_message", addMessage);
