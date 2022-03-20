d2cc37effc7f5296bb81655a5da15a89e34b076e
UHyun-K <konglim2@naver.com>
2022-03-20 17:04:54 +0900

    ice candidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -121,19 +121,39 @@ socket.on("welcome", async() => {//this code only loading first browser ..peerA
     socket.emit("offer", offer, roomName);
 });
 
-socket.on("offer",async (offer)=>{
+socket.on("offer",async (offer)=>{ //p2
+    console.log("received offer")
     myPeerConnection.setRemoteDescription(offer);
     const answer =  await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
 })
 
 socket.on("answer", (answer)=>{
+    console.log("received the answer")
     myPeerConnection.setRemoteDescription(answer);
 });
+
+socket.on("ice", (ice)=>{
+    console.log("received  candidate");
+    myPeerConnection.addIceCandidate(ice);
+})
 
 // RTC Code
 function   makeConnection(){
     myPeerConnection = new RTCPeerConnection(); //crate peer to peer connection
+    myPeerConnection.addEventListener("icecandidate", handleIce); //after making peerConnection , listen ice candidate
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream)); //put data inside peer connection
-    }
+}
+
+function handleIce(data){
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+
+}
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+  }
